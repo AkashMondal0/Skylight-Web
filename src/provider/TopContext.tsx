154acd2c340dae1775e097b621228d383204becf
Akash Mondal <akash2003modal@gmail.com>
2024-08-03 12:36:55 +0530

    fixed botton nav style, setup splash screen loading

--- src/provider/TopContext.tsx
+++ src/provider/TopContext.tsx
@@ -3,6 +3,8 @@ import React, { memo, useEffect, useState } from 'react'
 import { useTheme } from "next-themes"
 import { usePathname } from 'next/navigation'
 import { NavigationSidebar } from '@/components/Navigation/NavigationSidebar'
+import SplashScreen from '@/components/sky/SplashScreen'
+let splashShow = true
 
 const TopContext = memo(function TopContext({ children }: { children: React.ReactNode }) {
     const { theme } = useTheme()
@@ -21,22 +23,27 @@ const TopContext = memo(function TopContext({ children }: { children: React.Reac
             );
         }
         if (path === "/") {
-            return setNavigation((pre) => ({ ...pre, isHideNav: false, hideLabel: false }));
+            setNavigation((pre) => ({ ...pre, isHideNav: false, hideLabel: false }));
         } else if (path.includes("/message")) {
-            return setNavigation((pre) => ({ ...pre, isHideNav: false, hideLabel: true }));
+            setNavigation((pre) => ({ ...pre, isHideNav: false, hideLabel: true }));
         } else if (path.includes("/auth/")) {
-            return setNavigation((pre) => ({ ...pre, isHideNav: false }));
+            setNavigation((pre) => ({ ...pre, isHideNav: false }));
         } else {
-            return setNavigation((pre) => ({ ...pre, isHideNav: false, hideLabel: false }));
+            setNavigation((pre) => ({ ...pre, isHideNav: false, hideLabel: false }));
         }
+        if (splashShow) splashShow = false
+
     }, [theme, path])
 
-    return (<div className='w-full h-full flex'>
-        <NavigationSidebar
-            isHideNav={navigation.isHideNav}
-            hideLabel={navigation.hideLabel} />
-        {children}
-    </div>)
+    return (
+        <>
+            <SplashScreen show={splashShow} />
+            <div className='w-full h-full md:flex'>
+                <NavigationSidebar isHideNav={navigation.isHideNav} hideLabel={navigation.hideLabel} />
+                {children}
+            </div>
+        </>
+    )
 }, (() => true))
 
 export default TopContext
