9997a2330c75b1dfdfb3414c61cf8c423b8cea86
Akash Mondal <akash2003mondal.com>
2024-08-20 22:43:47 +0530

    fix: rerender and Update MessageSideBar and MessageUploadFile components

--- src/components/Message/MessageInput.tsx
+++ src/components/Message/MessageInput.tsx
@@ -1,13 +1,13 @@
 'use client';
 import { memo, useCallback, useContext, useMemo, useRef, useState } from 'react';
 import { Button } from '@/components/ui/button';
-import { Loader2Icon, Paperclip, Send } from 'lucide-react'
+import { Loader2Icon, Paperclip, PlusCircle, Send } from 'lucide-react'
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form'
 import { debounce } from 'lodash';
 import { useDispatch, useSelector } from 'react-redux'
-import { Assets, Conversation } from '@/types';
+import { Conversation } from '@/types';
 import { useSession } from 'next-auth/react';
 import { CreateMessageApi, fetchConversationsApi } from '@/redux/services/conversation';
 import { SocketContext } from '@/provider/Socket_Provider';
@@ -16,14 +16,17 @@ import { toast } from 'sonner';
 import { RootState } from '@/redux/store';
 import { FileUploadMenu } from '../Option/FileUploadMenu';
 import { UploadFileList } from './MessageUploadFile';
-const dropdownData = [{
-    label: "Photo",
-    onClick: () => document?.getElementById('files')?.click()
-},
-{
-    label: "Document",
-    onClick: () => { }
-}]
+
+const dropdownData = [
+    {
+        label: "Photo",
+        onClick: () => document?.getElementById('files')?.click()
+    },
+    {
+        label: "Document",
+        onClick: () => { }
+    }
+]
 const schema = z.object({
     message: z.string().min(1)
 })
@@ -31,12 +34,13 @@ export const MessageInput = memo(function MessageInput({ data }: { data: Convers
     const dispatch = useDispatch()
     const ConversationList = useSelector((state: RootState) => state.conversation.conversationList)
     const session = useSession().data?.user
-    const [assets, setAssets] = useState<Assets[]>([])
+    const [isFile, setIsFile] = useState<File[]>([])
     const [loading, setLoading] = useState(false)
     const socketState = useContext(SocketContext)
+
     const members = useMemo(() => {
         return data.members?.filter((i) => i !== session?.id) ?? []
-    }, [data.members])
+    }, [data.members, session?.id])
     const stopTypingRef = useRef(true)
 
     const { register, handleSubmit, reset, formState: { errors } } = useForm({
@@ -91,66 +95,72 @@ export const MessageInput = memo(function MessageInput({ data }: { data: Convers
             dispatch(fetchConversationsApi() as any)
         }
         reset()
-        setAssets([])
+        setIsFile([])
         setLoading(false)
     }
 
-    const onChangeFile = useCallback((e: any) => {
-        const files = e.target.files
-        if (files.length > 0) {
-            const _assets = Array.from(files).map((file: any) => {
-                return {
-                    _id: new Date().getTime().toString(),
-                    url: file,
-                    type: file.type.split('/')[0],
-                }
-            })
-            setAssets(_assets)
-        }
+    const onChangeFilePicker = (event: any) => {
+        let files = [...event.target.files]
+        isFile.map((file) => {
+            files = files.filter((f) => f.name !== file.name)
+        })
+        const stateImages = [...isFile, ...files]
+        setIsFile(stateImages)
+    }
+
+    const onRemoveItem = (id: string) => { 
+        setIsFile((prev) => prev.filter((i) => i.name !== id))
+    }
+
+    const onAddItem = useCallback(() => {
+        document?.getElementById('files')?.click()
     }, [])
 
 
 
     return (
         <>
-            <UploadFileList assets={assets} />
             <div className={`w-full border-t items-center
-             h-auto my-auto max-h-20 flex gap-1 sticky py-2 px-1
+             h-auto my-auto gap-1 sticky py-2 px-1
              bottom-0 z-50 bg-background`}>
-                <FileUploadMenu data={dropdownData}>
-                    <Button type="submit"
-                        variant={"outline"}
-                        className='rounded-3xl p-0 w-12'>
-                        <Paperclip />
-                    </Button>
-                </FileUploadMenu>
-                <input
-                    type="file"
-                    accept="image/*, video/*, audio/*"
-                    multiple
-                    name="file"
-                    id="files"
-                    className='hidden'
-                    onChange={(e) => { onChangeFile(e) }} />
-                <form onSubmit={handleSubmit(sendMessageHandle)}
-                    className="flex w-full items-center dark:bg-neutral-900
+                <UploadFileList assets={isFile} addItem={onAddItem} removeItem={onRemoveItem} />
+                {/* input */}
+                <div className='flex gap-2'>
+                    <FileUploadMenu data={dropdownData}>
+                        <Button type="submit"
+                            variant={"outline"}
+                            className='rounded-3xl p-0 w-12'>
+                            <Paperclip />
+                        </Button>
+                    </FileUploadMenu>
+                    <input
+                        type="file"
+                        accept="image/*, video/*, audio/*"
+                        multiple
+                        name="file"
+                        id="files"
+                        className='hidden'
+                        onChange={(e) => { onChangeFilePicker(e) }} />
+                    <form onSubmit={handleSubmit(sendMessageHandle)}
+                        className="flex w-full items-center dark:bg-neutral-900
                 bg-neutral-200 dark:text-neutral-100 text-neutral-800 rounded-3xl">
-                    <input id='message-input' className='outline-none focus:none 
+                        <input id='message-input' className='outline-none focus:none 
                     bg-transparent w-full p-2 dark:placeholder-neutral-100
                      placeholder-neutral-800' type="text" placeholder="Send a message"
-                        {...register("message", {
-                            required: true,
-                            onChange: onTyping,
-                        })}
-                    />
-                </form>
-                <Button type="submit"
-                    disabled={loading}
-                    onClick={handleSubmit(sendMessageHandle)}
-                    variant={"outline"}
-                    className='rounded-3xl h-10 px-3'>
-                    {loading ? <Loader2Icon className='animate-spin' /> : <Send />}
-                </Button>
+                            {...register("message", {
+                                required: true,
+                                onChange: onTyping,
+                            })}
+                        />
+                    </form>
+                    <Button type="submit"
+                        disabled={loading}
+                        onClick={handleSubmit(sendMessageHandle)}
+                        variant={"outline"}
+                        className='rounded-3xl h-10 px-3'>
+                        {loading ? <Loader2Icon className='animate-spin' /> : <Send />}
+                    </Button>
+                </div>
             </div>
         </>
     );
